ad64feb694b3b056b048c407047f0edb2e04ad20
2DOGZZzz <74018834+2DOGZZzz@users.noreply.github.com>
2026-06-01 12:06:24 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,14 @@
+const NameInput = document.getElementById('NameInput')
+const MarkInput = document.getElementById('MarkInput')
+const SubmitBtn= document.getElementById('SubmitBtn')
+
+
+function addName() {
+  if (NameInput.value=== ''){
+    alert(" Enter Student Name!")
+  }
+  else {
+    let li = document.createElement("li")
+    
+  }
+}
