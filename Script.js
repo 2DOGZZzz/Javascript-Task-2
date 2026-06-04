7abd274458ff2915d3ff4762c1c9db3fa93bb4d2
2DOGZZzz <74018834+2DOGZZzz@users.noreply.github.com>
2026-06-04 18:33:25 +0200

    updated source code

--- Script.js
+++ Script.js
@@ -1,61 +1,61 @@
-const NameInput = document.getElementById('NameInput').value.trim();
-const MarkInput = parseInt( document.getElementById('MarkInput').value);
-const SubmitBtn= document.getElementById('SubmitBtn');
-const Results = document.getElementById('Results')
+document.getElementById('studentform').addEventListener('submit', function (event)
+{
+ event.preventDefault();
+                
+ const NameInput = document.getElementById('NameInput');
+ const MarkInput = document.getElementById('MarkInput');
+ const SubmitBtn= document.getElementById('SubmitBtn');
+ const ResultsList = document.getElementById('Results')
 
 
-let Results = [];
-function getAllResults() {
-    
-    Results.innerHTML = "";
-    
-    tasks.forEach((task) =>{
-        const ResultCard = document.createElement('li');
-    ResultCard.Classlist.add('task')
-    
-        ResultCard.innerHTML = '<p ${task.title}</p>';
-     Results.appendChild(ResultCard)   
-        
-    })
-    
-    
-}
+ const name = NameInput.value.trim();
+ const mark = parseInt(MarkInput.value);
 
+                        
+ if (name === "" || isNaN(mark)) {
+                            
+ console.log('Please Enter the correct Student Information!!.');
+ return;
+ }
 
-getAllResults();
+                                                
+ if (mark < 0 || mark > 100) {
+                                                                            
+console.log('Please a number/ mark between 0 and 100.');
+return;
+ }
 
-function dataValidation(){
-    
-if (NameInput === "" || MarkInput === "") {
-    
-            console.log('Please Enter Student Name!!.');
-
-        }
-
-        const mark = parseFloat(MarkInput);
-
-        if (mark < 0 || mark > 100 || isNaN(mark)) {
-            
-            console.log('Please a number/ mark between 0 and 100.');
-        }
-        
 let response = "";
 
-        if (mark >= 80 && mark<=100) {
-            response = "Distinction";
-        } else if (mark >= 65 && mark<=79) {
-            response = "Merit";
-        } else if (mark >= 50 && mark<=64) {
-            response = "Pass";
-        } else {
-            response = "Fail";
-            
-        }        
+if (mark >= 80 && mark<=100) {
+                
+response = "Distinction";
+} 
+else if (mark >= 65 && mark<=79) {
+response = "Merit";
+                                                                                                                                                                            
+} 
+else if (mark >= 50 && mark<=64) {
+response = "Pass";
+} 
+else 
+{
+ response = "Fail";
+}        
+                                                                                                                                                                                                                                                                        
+const li = document.createElement('li');
+li.classList.add('task');
+ li.innerHTML = `
+ <div> <strong>Name:${name}, </strong> <strong>Mark: ${mark }%, </strong><strong>Outcome:${response}</strong> `;                                                                                                                                                                                                                                                                                                     
+                                                                                                                                                                                                                                                                                                                                
 
-console.log('Student Evaluation Outcome', NameInput, MarkInput, response);
+Resultslist.appendChild(li);
+NameInput.value = '';
+MarkInput.value = '';
+NameInput.focus();
+                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+});
 
-getAllResults();
 
-}
 
-SubmitBtn.addEventListener("click",dataValidation);
+
